c0a322e81cef4a053e7891b3efd9221aeb78f4db
pdubrovsk1y <pavel.dubrouski@innowise.com>
2025-06-02 07:44:53 +0200

    Useless logic removed

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
@@ -9,7 +7,5 @@ import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [ConfigModule, DatabaseModule, UsersModule, AuthModule, SeedModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
